71b74111fb06881545a318090d9c6a795c3dd281
AswiniB13 <79993990+AswiniB13@users.noreply.github.com>
2025-04-11 15:06:41 +0530

    Update outputs.tf

--- modules/windowsvm/outputs.tf
+++ modules/windowsvm/outputs.tf
@@ -2,8 +2,3 @@ output "vm_id" {
   description = "The ID of the Virtual Machine"
   value       = azurerm_virtual_machine.vm.id
 }
-
-output "vm_public_ip" {
-  description = "The public IP address of the Virtual Machine"
-  value       = azurerm_network_interface.nic.ip_configuration[0].public_ip_address_id
-}
