8ebf88d0a06b98bf51e579f5197b5490c34056fa
AswiniB13 <79993990+AswiniB13@users.noreply.github.com>
2025-04-11 16:18:45 +0530

    Update provider file

--- resources/networking/provider.tf
+++ resources/networking/provider.tf
@@ -1,5 +1,5 @@
 provider "azurerm" {
   features {}
   resource_provider_registrations = "none"
-  subscription_id = "52b0d977-da36-47f2-a3f1-4225c823ee20"
+  subscription_id = var.subscription_id
 }
