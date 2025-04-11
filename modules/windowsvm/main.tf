eaca8c5f600b03c5401fb25e35be4b711f05dbca
AswiniB13 <79993990+AswiniB13@users.noreply.github.com>
2025-04-11 14:52:28 +0530

    winvmmodule_updated

---
+++ modules/windowsvm/main.tf
@@ -0,0 +1,34 @@
+resource "azurerm_virtual_machine" "vm" {
+  name                  = var.name
+  location              = var.location
+  resource_group_name   = var.resource_group_name
+  network_interface_ids = [var.network_interface_id]
+  vm_size               = var.vm_size
+
+  storage_os_disk {
+    name              = "${var.name}-osdisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = var.os_disk_type
+  }
+
+  storage_image_reference {
+    publisher = var.image_publisher
+    offer     = var.image_offer
+    sku       = var.image_sku
+    version   = var.image_version
+  }
+
+  os_profile {
+    computer_name  = var.name
+    admin_username = var.admin_username
+    admin_password = var.admin_password
+  }
+
+  os_profile_windows_config {
+    provision_vm_agent        = true
+    enable_automatic_upgrades = true
+  }
+
+  tags = var.tags
+}
