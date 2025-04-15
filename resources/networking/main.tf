534569bdc83e3848dd1ecbe8968a5af8fb6f1a88
AswiniB13 <79993990+AswiniB13@users.noreply.github.com>
2025-04-15 19:31:42 +0530

    Update main.tf

--- resources/networking/main.tf
+++ resources/networking/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "ab-test-rg"
+    storage_account_name = "abteststorageacc13"
+    container_name       = "tfstatefile"
+    key                  = "dev.terraform.tfstate"
+  }
+}
 ### Creating Resource Group ##########
 module "resource_groups" {
   source = "github.com/AswiniB13/Azure-Terraform-Test//modules/resourcegroup/?ref=ab-feature-branch"
